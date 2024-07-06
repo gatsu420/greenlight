d576f384de3ce15ca255b35e639d33ec9b51a4c1
Ranggalawe Istifajar <ranggalawe.istifajar@wartek.belajar.id>
2024-07-07 03:36:41 +0700

    add error handling to handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,7 +15,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
-		w.WriteHeader(http.StatusNotFound)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -30,8 +30,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 }

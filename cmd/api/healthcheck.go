6984b62ba2c0237b6b70adef07beb9c74d493806
Ranggalawe Istifajar <ranggalawe.istifajar@wartek.belajar.id>
2024-07-07 02:30:56 +0700

    implement writeJSON to write json response body from map

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "%v", js)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
